5200372ee9ce4388090ce90ec6aaccda7ad4f0a6
ingeniarperuempresa <peruingeniar@gmail.com>
2024-10-30 15:42:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// Ruta para la raíz
+app.get('/', (req, res) => {
+    res.send('API de PideYA - Visita /consulta_todo para consultar la información.');
+});
+
 app.get('/consulta_todo', async (req, res) => {
-  try {
-    const response = await axios.get('https://sheetdb.io/api/v1/h6w2vg18zl5v0');
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al consultar SheetDB' });
-  }
+    try {
+        const response = await axios.get('https://sheetdb.io/api/v1/h6w2vg18zl5v0');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al consultar SheetDB' });
+    }
 });
 
 // Exportar la aplicación para Vercel
